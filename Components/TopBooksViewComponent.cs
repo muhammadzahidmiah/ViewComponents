3ecec69e237399e99675ff00d5baf75a50ae7a02
muhmmadzahidmiah <muhmmadzahidmiah@gmail.com>
2024-12-22 12:31:04 +0600

    Add project files.

---
+++ Components/TopBooksViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ViewComponents.Components
+{
+    public class TopBooksViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
